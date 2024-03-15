f435cf9d1ff20b8f9546de4c91d708a50f79f9a1
hutanmihai <hutanmihai29@gmail.com>
2024-03-15 18:05:32 +0200

    add discord bot notification

--- scraper/src/utils/decorators.py
+++ scraper/src/utils/decorators.py
@@ -1,5 +1,7 @@
 import timeit
 
+from discord import send_discord_notification
+
 
 def show_elapsed_time(func):
     def wrapper(*args, **kwargs):
@@ -8,6 +10,15 @@ def show_elapsed_time(func):
         end_time = timeit.default_timer()
         elapsed_time = end_time - start_time
         print(f"Elapsed time for {func.__name__}: {elapsed_time:.2f} seconds.")
+        return result
+
+    return wrapper
+
+
+def send_notification(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        send_discord_notification(f"Finished running {func.__name__}!")
         return result
 
     return wrapper
