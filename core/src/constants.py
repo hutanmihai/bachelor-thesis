3ee322a1a49d50fd732f39fb360ab8294b8bb3a9
hutanmihai <hutanmihai29@gmail.com>
2024-03-27 22:32:40 +0200

    update core

--- core/src/constants.py
+++ core/src/constants.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
 GRAPHS_PATH = Path("../graphs")
-DATA_PATH = Path("../data")
+DATA_PATH = Path("../../scraper/data/stage")
 
 IMAGES_PATH = DATA_PATH / "images"
 FINAL_CSV = DATA_PATH / "final.csv"
-CLEANED_CSV = DATA_PATH / "cleaned.csv"
-FORMATTED_CSV = DATA_PATH / "formatted.csv"
+CORE_CLEANED_CSV = DATA_PATH / "core_cleaned.csv"
+CORE_FORMATTED_CSV = DATA_PATH / "core_formatted.csv"
+
+ROCAR_CSV = DATA_PATH / "rocar.csv"
