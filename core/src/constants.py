9bed06b730db76fa82778821290388d101d2c166
hutanmihai <hutanmihai29@gmail.com>
2024-04-01 16:49:13 +0300

    big core update, updated to unique id zfill6 data update

--- core/src/constants.py
+++ core/src/constants.py
@@ -5,6 +5,7 @@ DATA_PATH = Path("../../scraper/data/stage")
 
 IMAGES_PATH = DATA_PATH / "images"
 FINAL_CSV = DATA_PATH / "final.csv"
+
 CORE_CLEANED_CSV = DATA_PATH / "core_cleaned.csv"
 CORE_FORMATTED_CSV = DATA_PATH / "core_formatted.csv"
 
