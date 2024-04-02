6b9fc7fe426f0e6bebd3be017a5b8104a34a9f2a
hutanmihai <hutanmihai29@gmail.com>
2024-04-02 18:47:33 +0300

    init rhf and zod

--- frontend/rocar-price-prediction/src/app/auth/login/page.tsx
+++ frontend/rocar-price-prediction/src/app/auth/login/page.tsx
@@ -5,20 +5,20 @@ function Login() {
   return (
     <main className="h-[calc(100vh-56px)] w-full lg:grid lg:grid-cols-2">
       <div className="flex items-center justify-center py-12">
-        <AuthForm type="login"/>
+        <AuthForm type="login" />
       </div>
-        <div className="hidden bg-muted lg:block">
-          <Image
-            src="/auth/side-image.webp"
-            alt="Image"
-            width="1024"
-            height="1024"
-            quality="100"
-            className="h-full w-full object-cover dark:brightness-[0.3]"
-          />
-        </div>
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/auth/side-image.webp"
+          alt="Image"
+          width="1024"
+          height="1024"
+          quality="100"
+          className="h-full w-full object-cover dark:brightness-[0.3]"
+        />
+      </div>
     </main>
-)
+  )
 }
 
 export default Login
